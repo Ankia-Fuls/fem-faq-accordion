f74b25a0c07cb6f52af19f11fda2eed99c502f58
Ankia-Fuls <ankiafuls01@gmail.com>
2025-08-25 16:21:01 +0200

    JS toggle

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggle = (header) => {
+    var panel = header.parentElement.nextElementSibling;
+    header.classList.toggle("active");
+
+    if (header.getAttribute("aria-expanded") === "true") {
+        header.setAttribute('aria-expanded', "false");
+        panel.setAttribute('aria-hidden', "true");
+    }
+    else {
+        header.setAttribute('aria-expanded', "true");
+        panel.setAttribute('aria-hidden', "false");
+    }
+
+}
