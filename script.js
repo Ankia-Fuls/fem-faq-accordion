9d7a7a1fe73f26933e01f67cf215418fba5d6288
Ankia-Fuls <ankiafuls01@gmail.com>
2025-08-25 17:00:27 +0200

    Icon toggle

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const toggle = (header) => {
     var panel = header.parentElement.nextElementSibling;
+    var image = header.getElementsByTagName("img");
+
     header.classList.toggle("active");
 
     if (header.getAttribute("aria-expanded") === "true") {
         header.setAttribute('aria-expanded', "false");
         panel.setAttribute('aria-hidden', "true");
-
+        image[0].src = "./assets/images/icon-plus.svg";
+        image[0].alt = "A purple plus icon";
     }
     else {
         header.setAttribute('aria-expanded', "true");
         panel.setAttribute('aria-hidden', "false");
 
-        header.lastChild.
+        image[0].src = "./assets/images/icon-minus.svg";
+        image[0].alt = "A dark purple minus icon";
     }
 
-    console.log(header.firstChild)
 }
